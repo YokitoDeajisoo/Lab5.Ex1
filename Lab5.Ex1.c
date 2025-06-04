456da4be2c915d56440f1001fcafd4b8177ad89c
Honcharuk Oleksii <oleksiugoncaruk@gmail.com>
2025-06-04 12:57:11 +0300

    Update Lab5.Ex1.c

--- Lab5.Ex1.c
+++ Lab5.Ex1.c
@@ -1,12 +1,8 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <clocale>
-
-void runManualInput();
-void runRandomGeneration();
-void processArray(int* a, int n);
 
 int main() {
     setlocale(LC_ALL, "Ukrainian");
@@ -20,17 +16,47 @@ int main() {
         printf("Ваш вибір: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-        case 1:
-            runManualInput();
-            break;
-        case 2:
-            runRandomGeneration();
-            break;
-        case 3:
+        if (choice == 1 || choice == 2) {
+            int n;
+            printf("Введiть кiлькiсть елементiв масиву: ");
+            scanf("%d", &n);
+
+            if (n <= 0) {
+                printf("Неприпустима довжина масиву.\n");
+                continue;
+            }
+
+            int a[1000]; // обмеження, замість dynamic malloc
+
+            if (choice == 1) {
+                for (int i = 0; i < n; i++) {
+                    printf("a[%d] = ", i);
+                    scanf("%d", &a[i]);
+                }
+            }
+            else if (choice == 2) {
+                srand((unsigned int)time(NULL));
+                printf("Згенерований масив:\n");
+                for (int i = 0; i < n; i++) {
+                    a[i] = rand() % 201 - 100;
+                    printf("a[%d] = %d\n", i, a[i]);
+                }
+            }
+
+            int min = a[0], max = a[0];
+            for (int i = 1; i < n; i++) {
+                if (a[i] < min) min = a[i];
+                if (a[i] > max) max = a[i];
+            }
+
+            int diff = abs(max - min);
+            printf("Максимум = %d, Мiнiмум = %d\n", max, min);
+            printf("Модуль рiзницi мiж мiнiмальним i максимальним: %d\n", diff);
+        }
+        else if (choice == 3) {
             printf("Завершення програми.\n");
-            break;
-        default:
+        }
+        else {
             printf("Невiрний вибiр. Спробуйте ще раз.\n");
         }
     } while (choice != 3);
@@ -38,68 +64,3 @@ int main() {
     return 0;
 }
 
-void runManualInput() {
-    int n;
-    printf("Введiть кiлькiсть елементiв масиву: ");
-    scanf("%d", &n);
-
-    if (n <= 0) {
-        printf("Неприпустима довжина масиву.\n");
-        return;
-    }
-
-    int* a = (int*)malloc(n * sizeof(int));
-    if (a == NULL) {
-        printf("Помилка видiлення пам’ятi.\n");
-        return;
-    }
-
-    for (int i = 0; i < n; i++) {
-        printf("a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
-
-    processArray(a, n);
-    free(a);
-}
-
-void runRandomGeneration() {
-    int n;
-    printf("Введiть кiлькiсть елементiв масиву: ");
-    scanf("%d", &n);
-
-    if (n <= 0) {
-        printf("Неприпустима довжина масиву.\n");
-        return;
-    }
-
-    int* a = (int*)malloc(n * sizeof(int));
-    if (a == NULL) {
-        printf("Помилка видiлення пам’яті.\n");
-        return;
-    }
-
-    srand((unsigned int)time(NULL));
-
-    printf("Згенерований масив:\n");
-    for (int i = 0; i < n; i++) {
-        a[i] = rand() % 201 - 100;
-        printf("a[%d] = %d\n", i, a[i]);
-    }
-
-    processArray(a, n);
-    free(a);
-}
-
-void processArray(int* a, int n) {
-    int min = a[0], max = a[0];
-
-    for (int i = 1; i < n; i++) {
-        if (a[i] < min) min = a[i];
-        if (a[i] > max) max = a[i];
-    }
-
-    int diff = abs(max - min);
-    printf("Максимум = %d, Мiнiмум = %d\n", max, min);
-    printf("Модуль рiзниці мiж мiнiмальним i максимальним: %d\n", diff);
-}
